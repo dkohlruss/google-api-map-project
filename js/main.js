1d9b4adbf03b498d528202d70ffdcb6a185507cb
David <dkohlruss@gmail.com>
2017-02-07 18:31:56 -0700

    Remove minor spaghettification

--- js/main.js
+++ js/main.js
@@ -52,28 +52,106 @@ var locations = [
 	}
 ];
 
-// Global functions
-var map;
-var markers;
-var infoWindow;
-
 // Google Map Initialization
 function initMap() {
-   	map = new google.maps.Map(document.getElementById('map'), {
-		center: {lat: 51.0545169, lng: -114.0860707 },
-		zoom: 17
-	});
-
 	ko.applyBindings(ViewModel());
 }
 
 // Knockout ViewModel
 function ViewModel() {
 	var self = this;
-	infoWindow = new google.maps.InfoWindow();
+    var map;
+    var markers;
+    var infoWindow = new google.maps.InfoWindow();
 	self.sortedlocations = ko.observableArray(locations);
 
-	// Create markers and add infowindow functionality
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: 51.0545169, lng: -114.0860707 },
+        zoom: 17,
+        styles: [
+            {elementType: 'geometry', stylers: [{color: '#242f3e'}]},
+            {elementType: 'labels.text.stroke', stylers: [{color: '#242f3e'}]},
+            {elementType: 'labels.text.fill', stylers: [{color: '#746855'}]},
+            {
+                featureType: 'administrative.locality',
+                elementType: 'labels.text.fill',
+                stylers: [{color: '#d59563'}]
+            },
+            {
+                featureType: 'poi',
+                elementType: 'labels.text.fill',
+                stylers: [{color: '#d59563'}]
+            },
+            {
+                featureType: 'poi.park',
+                elementType: 'geometry',
+                stylers: [{color: '#263c3f'}]
+            },
+            {
+                featureType: 'poi.park',
+                elementType: 'labels.text.fill',
+                stylers: [{color: '#6b9a76'}]
+            },
+            {
+                featureType: 'road',
+                elementType: 'geometry',
+                stylers: [{color: '#38414e'}]
+            },
+            {
+                featureType: 'road',
+                elementType: 'geometry.stroke',
+                stylers: [{color: '#212a37'}]
+            },
+            {
+                featureType: 'road',
+                elementType: 'labels.text.fill',
+                stylers: [{color: '#9ca5b3'}]
+            },
+            {
+                featureType: 'road.highway',
+                elementType: 'geometry',
+                stylers: [{color: '#746855'}]
+            },
+            {
+                featureType: 'road.highway',
+                elementType: 'geometry.stroke',
+                stylers: [{color: '#1f2835'}]
+            },
+            {
+                featureType: 'road.highway',
+                elementType: 'labels.text.fill',
+                stylers: [{color: '#f3d19c'}]
+            },
+            {
+                featureType: 'transit',
+                elementType: 'geometry',
+                stylers: [{color: '#2f3948'}]
+            },
+            {
+                featureType: 'transit.station',
+                elementType: 'labels.text.fill',
+                stylers: [{color: '#d59563'}]
+            },
+            {
+                featureType: 'water',
+                elementType: 'geometry',
+                stylers: [{color: '#17263c'}]
+            },
+            {
+                featureType: 'water',
+                elementType: 'labels.text.fill',
+                stylers: [{color: '#515c6d'}]
+            },
+            {
+                featureType: 'water',
+                elementType: 'labels.text.stroke',
+                stylers: [{color: '#17263c'}]
+            }
+        ]
+    });
+
+
+    // Create markers and add infowindow functionality
 	self.sortedlocations().forEach(function(location) {
 		var position = location.latlng;
 		var title = location.name;
